5711d8bcc2758d852f0d5d4833825d6b3557fc8f
Nick <3816295+nickreynke@users.noreply.github.com>
2025-04-10 15:31:53 +0200

    feat(nodejs): additional attributes for rollDice span redaction
    Added detailed user attributes (name, email, ID, office) to the rollDice span for redaction.

--- examples/nodejs/app.js
+++ examples/nodejs/app.js
@@ -12,10 +12,18 @@ const PORT = parseInt(process.env.PORT || '8084')
 const app = express()
 
 app.get('/rolldice', (req, res) => {
-  return tracer.startActiveSpan('rollDice', (span) => {
-    const randomName = faker.person.fullName();
-    const randomEmail = faker.internet.email();
-    const randomUUID = faker.string.uuid();
+  const randomName = faker.person.fullName();
+  const randomEmail = faker.internet.email();
+  const randomUUID = faker.string.uuid();
+
+  return tracer.startActiveSpan('rollDice', {
+    attributes: {
+      'user.name': randomName,
+      'user.email': randomEmail,
+      'user.id': randomUUID,
+      'user.office': 'Datadrivers GmbH'
+    }
+  }, (span) => {
     const rolls = req.query.rolls ? parseInt(req.query.rolls.toString()) : NaN
     logger.log('Received request to dice ' + rolls + ' rolls for UUID ' + randomUUID)
     if (isNaN(rolls)) {
@@ -31,6 +39,7 @@ app.get('/rolldice', (req, res) => {
       return
     }
     const result = JSON.stringify([rollTheDice(rolls, 1, 6), randomUUID, randomName, randomEmail])
+    logger.log('Result: ' + result);
     span.end()
     res.send(result)
   })
